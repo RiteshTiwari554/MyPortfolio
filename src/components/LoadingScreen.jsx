f62ab07b89e0fb4faed48daaa342cdcd4c96481d
Ritesh Tiwari <84981640+RiteshTiwari554@users.noreply.github.com>
2024-04-15 14:02:40 +0530

    Update LoadingScreen.jsx

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -5,8 +5,10 @@ const LoadingScreen = () => {
     <div className="flex justify-center items-center h-screen">
       <div>
         <img
+          
           src="https://cdn.dribbble.com/users/450343/screenshots/2465762/media/97b14be7af51dcea4d8ae244c545769b.gif"
           alt=""
+          
         />
       </div>
     </div>
